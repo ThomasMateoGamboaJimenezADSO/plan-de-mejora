19eee289e8eb42e3d15dfffa515b9fbaad630913
thomas mateo <tgamboa456@gmail.com>
2024-06-11 09:29:15 -0500

    terminado

--- ejercicios/ejercicio_3.js
+++ ejercicios/ejercicio_3.js
@@ -1,4 +1,21 @@
 // Dividir dos números sin el operador de la división
 
-let num1 = parseInt(prompt(' digite el diivisor '));
-let num2 = parseInt(prompt(' digite el dividendo'));
+let cociente = 0
+let num1 = parseInt(prompt(' digite el dividendo '));
+let num2 = parseInt(prompt(' digite el divisor (recuerde que este valor debe ser menor al valor anteriormente ingresado) '));
+
+if (num1 > num2) {
+  while (num1 > 0) {
+    cociente++;
+    num1 = num1 - num2
+  }
+  console.log(' sin operador ', cociente)
+}
+
+else {
+  alert(' ERROR LOS VALORES SON IMCOMPATIBLES ');
+}
+
+let revicion = num1 / num2
+
+console.log(' con operador ', revicion);
